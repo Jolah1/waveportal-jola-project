e880c6216ecea7a2b4d0ccbf431eb869bc7909ff
okunlade Jolade <jokunlade@gmail.com>
2022-05-25 23:50:16 +0100

    still adding functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import * as React from "react";
+import React, {useEffect, useState} from "react";
 import './App.css';
 
-export default function App() {
+const App = () => {
 
-  const wave = () => {
-    
+  /*
+  *just a state variable we use to store our user's public wallet"
+  */
+const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+  
+    const accounts = await ethereum.request({method: "eth_accounts"});
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account)
+    } else {
+      console.log("No authorized account found")
+    }
+  } catch (error) {
+    console.log(error);
   }
+}
+
+  useEffect (() => {
+    checkIfWalletIsConnected();
+  }, [])
+    
   
   return (
     <div className="mainContainer">
@@ -19,10 +50,12 @@ export default function App() {
         I am Jolade and I am a solidity developer, that's pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={wave}>
+        <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
       </div>
     </div>
   );
 }
+
+export default App
