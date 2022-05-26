ab05a0dc3e50843cd563333ff4f0a5ce06df6ea8
okunlade Jolade <jokunlade@gmail.com>
2022-05-26 12:36:29 +0100

    textarea added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,36 @@ const App = () => {
 
 const [currentAccount, setCurrentAccount] = useState("");
 
-const contractAddress = "0x7D6943f7412E5e7625b849b885d2d137f9ed1e5a";
+const [allWaves, setAllWaves] = useState([]);
+const contractAddress = "0x062Bc262F0EC9aACc86a8d6fBaf1D9387be2f643";
+
+const getAllWaves = async () => {
+  try {
+    const {ethereum} = window;
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+      const waves = await wavePortalContract.getAllWaves();
+
+      let wavesCleaned = [];
+      waves.forEach(wave => {
+        wavesCleaned.push({
+          address: wave.waver,
+          timestamp: new Date(wave.timestamp * 1000),
+          message: wave.message
+        });
+      });
+
+      setAllWaves(wavesCleaned);
+    } else {
+      console.log("Ethereum object doesn't exist!")
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 const contractABI = abi.abi;
 
@@ -17,9 +46,9 @@ const contractABI = abi.abi;
 
     if (!ethereum) {
       console.log("Make sure you have metamask!");
-      return;
+      return getAllWaves;
     } else {
-      console.log("We have the ethereum object", ethereum);
+    console.log("We have the ethereum object", ethereum);
     }
   
     const accounts = await ethereum.request({method: "eth_accounts"});
@@ -63,7 +92,7 @@ const connectWallet = async () => {
           let count = await wavePortalContract.getTotalWaves();
           console.log("Retrieved total wave count...", count.toNumber());
         
-        const waveTxn = await wavePortalContract.wave();
+        const waveTxn = await wavePortalContract.wave("this is a message");
         console.log("Mining...", waveTxn.hash);
 
         await waveTxn.wait();
@@ -80,8 +109,6 @@ const connectWallet = async () => {
         }
     }
 
-   
-
   useEffect (() => {
     checkIfWalletIsConnected();
   }, [])
@@ -96,7 +123,8 @@ const connectWallet = async () => {
         </div>
 
         <div className="bio">
-        I am Jolade and I am a solidity developer, that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        I am Jolade and I am a solidity developer, that's pretty cool right? Connect your Ethereum wallet and wave at me! <br />
+        Please ensure your Metamask wallet is on Rinkeby Test Network.
         </div>
 
         <button className="waveButton" onClick={wave}>
@@ -108,8 +136,24 @@ const connectWallet = async () => {
             Connect Wallet
           </button>
         )}
+
+        {allWaves.map((wave, index) => {
+          return (
+            <div key={index} style={{backgroundColor: "OldLace", marginTop: "16px", padding: "8px" }}>
+              <div>Address: {wave.address}</div>
+              <div>Time: {wave.timestamp.toString()}</div>
+              <div>Message: {wave.message}</div>
+            </div>)
+        })}
+        <div>
+        <label For="Message:"></label>
+        <textarea name="message" id="message" cols="30" rows="10"></textarea>
+
+        </div>
       </div>
+     
     </div>
+    
   );
 }
 
