08bfec24f32e9b1289ae65148db1721c6cea75f5
okunlade Jolade <jokunlade@gmail.com>
2022-05-27 13:15:22 +0100

    done for now

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ const [allWaves, setAllWaves] = useState([]);
 const contractAddress = "0xe83Be76289C8310E30e29DCBb6322E8921d7322D";
 
 const getAllWaves = async () => {
+  const {ethereum} = window;
   try {
-    const {ethereum} = window;
     if (ethereum) {
       const provider = new ethers.providers.Web3Provider(ethereum);
       const signer = provider.getSigner();
@@ -20,13 +20,13 @@ const getAllWaves = async () => {
 
       const waves = await wavePortalContract.getAllWaves();
 
-      let wavesCleaned = [];
-      waves.forEach(wave => {
-        wavesCleaned.push({
+      const wavesCleaned = waves.map(wave => {
+        return {
+
           address: wave.waver,
           timestamp: new Date(wave.timestamp * 1000),
           message: wave.message
-        });
+        };
       });
 
       setAllWaves(wavesCleaned);
@@ -36,7 +36,7 @@ const getAllWaves = async () => {
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 const contractABI = abi.abi;
 
@@ -63,7 +63,37 @@ const contractABI = abi.abi;
   } catch (error) {
     console.log(error);
   }
+};
+
+useEffect(() => {
+  let wavePortalContract;
+
+  const onNewWave = (from, timestamp, message) => {
+    console.log("NewWave", from, timestamp, message);
+  setAllWaves(prevState => [
+    ...prevState,
+    {
+      address: from,
+      timestamp: new Date(timestamp * 1000),
+      message: message,
+    },
+  ]);
+};
+
+if (window.ethereum) {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+
+  wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+    wavePortalContract.on("NewWave", onNewWave);
 }
+
+return () => {
+  if (wavePortalContract) {
+    wavePortalContract.off("NewWave", onNewWave);
+  }
+  };
+}, []);
 
 const connectWallet = async () => {
   try {
@@ -92,7 +122,7 @@ const connectWallet = async () => {
           let count = await wavePortalContract.getTotalWaves();
           console.log("Retrieved total wave count...", count.toNumber());
         
-        const waveTxn = await wavePortalContract.wave("this is a message");
+        const waveTxn = await wavePortalContract.wave(message, { gasLimit: 300000 });
         console.log("Mining...", waveTxn.hash);
 
         await waveTxn.wait();
